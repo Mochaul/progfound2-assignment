a4477d6959955922caada88ccc94867329ecba5b
Nicolaus Christian <nicolauscg@gmail.com>
2018-04-06 20:49:36 +0700

    fix initial arrangement of cages

--- assignment-2/src/main/java/cages/CageArrangement.java
+++ assignment-2/src/main/java/cages/CageArrangement.java
@@ -10,12 +10,18 @@ public class CageArrangement{
         if (animals.length > 0) this.location = Cage.determineCage(animals[0]).getLocation();
         for (int a=0; a<3; a++) this.content[a] = new ArrayList<Cage>();
         for(int i=0; i < animals.length; i++){
-            if (i < animals.length/3){
-                this.content[2].add(Cage.determineCage(animals[i]));
-            }else if (i < 2*animals.length/3){
-                this.content[1].add(Cage.determineCage(animals[i]));
+            if (animals.length >= 3){
+                if (i < animals.length/3){
+                    this.content[2].add(Cage.determineCage(animals[i]));
+                }else if (i < 2*animals.length/3){
+                    this.content[1].add(Cage.determineCage(animals[i]));
+                }else{
+                    this.content[0].add(Cage.determineCage(animals[i]));
+                }
             }else{
-                this.content[0].add(Cage.determineCage(animals[i]));
+                for(int j = 0; j<animals.length; j++){
+                    this.content[2-j].add(Cage.determineCage(animals[j]));
+                }
             }
         }
     }
