ac814605e3708b9d88b1c55235e0063c28b32377
Daya Adianto <dayaadianto@cs.ui.ac.id>
2018-04-16 18:40:18 +0700

    Document RegistrationWriter class

--- assignment-3/src/main/java/javari/writer/RegistrationWriter.java
+++ assignment-3/src/main/java/javari/writer/RegistrationWriter.java
@@ -13,11 +13,26 @@ import javari.park.SelectedAttraction;
 
 import org.json.JSONWriter;
 
+/**
+ * This class handles saving data of registered attractions into JSON file.
+ *
+ * @author Programming Foundations 2 Teaching Team
+ * @author TODO If you make changes in this class, please write your name here
+ *     and describe the changes in the comment block
+ */
 public class RegistrationWriter {
 
     private static final String DEFAULT_FILENAME_FMT =
             "registration_%s.json";
 
+    /**
+     * Writes visitor's registration data into JSON file.
+     *
+     * @param registration  instance of registration that contains attractions
+     *                      that will be watched by visitor
+     * @param directory     the directory where JSON file will be written
+     * @throws IOException  when JSON file cannot be written
+     */
     public static void writeJson(Registration registration,
                                  Path directory) throws IOException {
         String fileName = String.format(DEFAULT_FILENAME_FMT,
@@ -28,6 +43,8 @@ public class RegistrationWriter {
                 StandardCharsets.UTF_8);
         final JSONWriter writer = new JSONWriter(fileWriter);
 
+        // Note: JSONWriter is following Builder design pattern
+        // to provide a "fluent-style" JSON file creation
         writer.array();
         buildRegistration(writer, registration);
         writer.endArray();
@@ -35,10 +52,19 @@ public class RegistrationWriter {
         System.out.println("... End of program, write to "
                 + jsonFile.getFileName().toString());
 
+        // Ensure JSON text are flushed out from internal buffer
+        // into the actual file
         fileWriter.flush();
         fileWriter.close();
     }
 
+    /**
+     * Uses call-by-reference to write content of registration using
+     * given instance of JsonWriter.
+     *
+     * @param writer        an instance of JSONWriter passed by reference
+     * @param registration  registration object
+     */
     private static void buildRegistration(JSONWriter writer,
                                           Registration registration) {
         writer.object()
@@ -53,6 +79,14 @@ public class RegistrationWriter {
         writer.endArray().endObject();
     }
 
+    /**
+     * Uses call-by-reference to write an instance of selected attraction into
+     * intermediate result of JSON file writing.
+     *
+     * @param writer        instance of JSONWriter containing intermediate result
+     * @param attraction    an attraction that will be watched by visitor according
+     *                      to the registration info
+     */
     private static void buildAttraction(JSONWriter writer,
                                         SelectedAttraction attraction) {
         String type = attraction.getType();
@@ -66,6 +100,14 @@ public class RegistrationWriter {
         writer.endObject();
     }
 
+    /**
+     * Uses call-by-reference to write list of animal performers into
+     * intermediate result of JSON writing.
+     *
+     * @param writer    instance of JSONWriter containing intermediate result
+     *                  of JSON writing
+     * @param animals   list of animals that perform in the attraction
+     */
     private static void buildPerformers(JSONWriter writer,
                                         List<Animal> animals) {
         writer.key("animals").array();
@@ -73,6 +115,14 @@ public class RegistrationWriter {
         writer.endArray();
     }
 
+    /**
+     * Uses call-by-reference to write an animal into intermediate result
+     * of JSON writing.
+     *
+     * @param writer    instance of JSONWriter containing intermediate result
+     *                  of JSON writing
+     * @param animal    an instance of animal that will perform in an attraction
+     */
     private static void buildPerformer(JSONWriter writer,
                                        Animal animal) {
         writer.object()
