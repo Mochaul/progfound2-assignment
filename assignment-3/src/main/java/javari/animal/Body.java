b8bd47cc38ed5aa44214266578df0aa447c55ea2
Daya Adianto <dayaadianto@cs.ui.ac.id>
2018-04-16 18:07:08 +0700

    Document Body class

--- assignment-3/src/main/java/javari/animal/Body.java
+++ assignment-3/src/main/java/javari/animal/Body.java
@@ -1,11 +1,25 @@
 package javari.animal;
 
+/**
+ * This class describes bodily features in an animal.
+ *
+ * @author Programming Foundations 2 Teaching Team
+ * @author TODO If you make changes in this class, please write your name here
+ *     and describe the changes in the comment block
+ */
 public class Body {
 
     private double length;
     private double weight;
     private Gender gender;
 
+    /**
+     * Constructs an instance of {@code Body} that part of an {@code Animal}.
+     *
+     * @param length length of animal in centimeters
+     * @param weight weight of animal in kilograms
+     * @param gender gender of animal (male/female)
+     */
     public Body(double length, double weight, Gender gender) {
         this.length = length;
         this.weight = weight;
